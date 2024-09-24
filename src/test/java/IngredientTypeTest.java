977dc8c7a32b807bd7b3c4a95878d8f895187765
Nikolai Roleder <nik_roleder95@mail.ru>
2024-09-24 19:59:20 +0300

    fix pom and param test

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,13 +1,36 @@
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.IngredientType;
-import java.util.EnumSet;
-import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
-    @ParameterizedTest
-    @EnumSource(value = IngredientType.class, names = {"SAUCE", "FILLING"})
-    public void testIngredientTypeValues(IngredientType ingredientType) {
-        assertTrue(EnumSet.allOf(IngredientType.class).contains(ingredientType));
+
+    private IngredientType type;
+    private String expectedName;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {IngredientType.SAUCE, "SAUCE"},
+                {IngredientType.FILLING, "FILLING"}
+        });
     }
+
+    public IngredientTypeTest(IngredientType type, String expectedName) {
+        this.type = type;
+        this.expectedName = expectedName;
+    }
+
+    @Test
+    public void testIngredientTypeValues() {
+        String actualName = type.toString();
+        assertEquals(expectedName, actualName);
+    }
+
 }
